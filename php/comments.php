545ece3d43aff448f57766ba0b091e6351427bd3
wladimirrs <185645045+wladimirrs@users.noreply.github.com>
2026-02-23 10:48:13 +0100

    PHP-Grundlagen erlernen Schritt 1

--- php/comments.php
+++ php/comments.php
@@ -3,18 +3,18 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Startseite</title>
+    <title>Kommentare</title>
 </head>
 
 
 <body>
-    <p1>PHP-Variablen</p1>
     <?php
-    //Variablen beginnen mit $, alphanumerisch
-
-    $eineZahl = 0;
-    $EINEZahl = 5;
+    echo "Hier stehen Kommentare";
     ?>
-    
+
+    <?php
+    echo "Datum: " . date("d.m.Y");
+    ?>
+
 </body>
 </html>
